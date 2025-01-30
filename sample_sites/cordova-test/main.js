8895debc77f8fd98c9485068b1a321e5013b8dca
hweij <henk000@gmail.com>
2025-01-30 17:01:06 +0100

    feat: add on-display logging

--- sample_sites/cordova-test/main.js
+++ sample_sites/cordova-test/main.js
@@ -2,9 +2,17 @@
 
 function getRootDir() {
     const loc = window.location;
-    console.log("LOCATION");
-    console.log(loc);
+    LOG("LOCATION");
+    LOG(loc);
     return loc.href.replace("/index.html", "/");
+}
+
+function LOG(s) {
+    const win = document.getElementById("divLogs");
+    if (win) {
+        win.insertAdjacentText("beforeend", `${s}\n`);
+    }
+    console.log(s);
 }
 
 /**
@@ -12,30 +20,63 @@ function getRootDir() {
  */
 function loadTextAsset(fpath) {
     if (_cordovaActive) {
-        return loadLocalAsset(fpath, "string");
+        return new Promise(
+            /**
+             * @param {(v: string) => void} resolve
+             * @param {(v: string) => void} reject
+             */
+            (resolve, reject) => {
+                corLoadLocalFile(fpath, "string",
+                    v => {
+                        if (v instanceof ArrayBuffer) {
+                            throw new Error("Unexpected return type");
+                        }
+                        else {
+                            resolve(v);
+                        }
+                    },
+                    err => {
+                        reject(err);
+                    }
+                );
+            }
+        );
     }
     else {
         return fetch(fpath).then(res => res.text());
     }
 }
 
+/**
+ * @param {string} fpath
+ */
 function loadBufferAsset(fpath) {
     if (_cordovaActive) {
-        return loadLocalAsset(fpath, "buffer");
+        return new Promise(
+            /**
+             * @param {(v: ArrayBuffer) => void} resolve
+             * @param {(v: string) => void} reject
+             */
+            (resolve, reject) => {
+                corLoadLocalFile(fpath, "buffer",
+                    v => {
+                        if (v instanceof ArrayBuffer) {
+                            resolve(v);
+                        }
+                        else {
+                            throw new Error("Unexpected return type");
+                        }
+                    },
+                    err => {
+                        reject(err);
+                    }
+                );
+            }
+        );
     }
     else {
         return fetch(fpath).then(res => res.arrayBuffer());
     }
-}
-
-/**
- *
- * @param {string} fpath
- * @param {"string" | "buffer"} format;
- */
-async function loadLocalAsset(fpath, format) {
-    const res = corLoadLocalFile(getRootDir() + fpath, format);
-    return res;
 }
 
 // Wait for the deviceready event before using any of Cordova's device APIs.
@@ -46,71 +87,60 @@ var _cordovaActive = false;
 
 function onDeviceReady() {
     _cordovaActive = true;
-    console.log("Device ready");
-    console.log("Load asset TEST");
-    loadTextAsset("index.html").then((v) => console.log(v));
+    LOG("Device ready");
+    LOG("Load asset TEST2");
+    loadTextAsset("index.html").then((v) => LOG(v));
 }
 
 /**
- *
- * @param {string} fpath
- * @param {"string" | "buffer"} format;
+ * @param {string} rpath
+ * @param {"string" | "buffer"} format
+ * @param {(v: string | ArrayBuffer) => void} cb
+ * @param {(v: string) => void} err
  */
-function corLoadLocalFile(fpath, format) {
-    return new Promise(
-        /**
-         * @param {(v: string | ArrayBuffer) => void} resolve
-         * @param {(v: string) => void} reject
-         */
-        (resolve, reject) => {
-            console.log(`Loading local file ${fpath}`);
-            window.resolveLocalFileSystemURL(
-                fpath,
-                e => {
-                    if (e.isFile) {
-                        /** @type FileEntry */(e).file(file => {
-                        console.log("success"); console.log(e);
+function corLoadLocalFile(rpath, format, cb, err) {
+    const fpath = getRootDir() + rpath;
+    LOG(`Loading local file ${fpath}`);
+    window.resolveLocalFileSystemURL(
+        fpath,
+        e => {
+            if (e.isFile) {
+                /** @type FileEntry */(e).file(
+                file => {
+                    LOG("success"); LOG(e);
 
-                        var reader = new FileReader();
+                    var reader = new FileReader();
 
-                        reader.onloadend = function (e) {
-                            if (format === "buffer") {
-                                console.log("Read binary file");
-                            }
-                            else {
-                                console.log("Read text file");
-                            }
-                            if (this.result !== null) {
-                                resolve(this.result);
-                            }
-                            else {
-                                reject("Received empty result");
-                            }
-                        }
-
-                        reader.onerror = function (e) {
-                            console.log(`Error for`);
-                            console.log(this.result);
-                            reject(`Error reading file ${fpath}`);
-                        }
-
-                        if (format === "buffer") {
-                            reader.readAsArrayBuffer(file);
+                    reader.onloadend = function (e) {
+                        if (this.result !== null) {
+                            cb(this.result);
                         }
                         else {
-                            reader.readAsText(file);
+                            err("Received empty result");
                         }
-                    });
+                    }
+
+                    reader.onerror = function (e) {
+                        LOG(`Error for`);
+                        LOG(this.result);
+                        err(`Error reading file ${fpath}`);
+                    }
+
+                    if (format === "buffer") {
+                        reader.readAsArrayBuffer(file);
                     }
                     else {
-                        reject(`Incorrect file: ${fpath}`);
+                        reader.readAsText(file);
                     }
-                },
-                err => {
-                    console.log("fail");
-                    console.log(err);
-                    reject(`Error reading file ${fpath}`);
                 });
-        }
-    );
+            }
+            else {
+                err(`Incorrect file: ${fpath}`);
+            }
+        },
+        e => {
+            LOG("fail");
+            LOG(e);
+            err(`Error reading file ${fpath}`);
+        });
 }
