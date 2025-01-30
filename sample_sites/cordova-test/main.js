66575797f862efd3c9b3f126390bb5ea33c100b0
hweij <henk000@gmail.com>
2025-01-30 15:57:37 +0100

    feat: create draft promise-based asset loader

--- sample_sites/cordova-test/main.js
+++ sample_sites/cordova-test/main.js
@@ -20,12 +20,15 @@ function loadDataAsset(fpath) {
  * @param {string} fpath
  * @param {"string" | "buffer"} format;
  */
-function loadLocalAsset(fpath, format) {
+async function loadLocalAsset(fpath, format) {
     console.log("Window props:")
     if (_cordovaActive) {
         console.log(`Cordova present`);
         console.log(cordova);
-        corLoadLocalFile(getRootDir() + fpath, format);
+        const res = await corLoadLocalFile(getRootDir() + fpath, format);
+        console.log("PROMISE");
+        console.log(res);
+        return res;
     }
     else {
         console.log("Cordova not present");
@@ -50,39 +53,56 @@ function onDeviceReady() {
  * @param {"string" | "buffer"} format;
  */
 function corLoadLocalFile(fpath, format) {
-    console.log(`Loading local file ${fpath}`);
-    window.resolveLocalFileSystemURL(
-        fpath,
-        e => {
-            if (e.isFile) {
-                /** @type FileEntry */(e).file(file => {
-                console.log("success"); console.log(e);
+    return new Promise(
+        /**
+         *
+         * @param {(v: string | ArrayBuffer | null) => void} resolve
+         * @param {(v: string) => void} reject
+         */
+        (resolve, reject) => {
+            console.log(`Loading local file ${fpath}`);
+            window.resolveLocalFileSystemURL(
+                fpath,
+                e => {
+                    if (e.isFile) {
+                        /** @type FileEntry */(e).file(file => {
+                        console.log("success"); console.log(e);
 
-                var reader = new FileReader();
+                        var reader = new FileReader();
 
-                reader.onloadend = function (e) {
-                    if (format === "buffer") {
-                        console.log("Read binary file");
+                        reader.onloadend = function (e) {
+                            if (format === "buffer") {
+                                console.log("Read binary file");
+                            }
+                            else {
+                                console.log("Read text file");
+                            }
+                            resolve(this.result);
+                        }
+
+                        reader.onerror = function (e) {
+                            console.log(`Error for`);
+                            console.log(this.result);
+                            reject(`Error reading file ${fpath}`);
+                        }
+
+                        if (format === "buffer") {
+                            reader.readAsArrayBuffer(file);
+                        }
+                        else {
+                            reader.readAsText(file);
+                        }
+                    });
                     }
                     else {
-                        console.log("Read text file");
+                        reject(`Incorrect file: ${fpath}`);
                     }
-                    console.log(this.result);
-                }
-
-                reader.onerror = function (e) {
-                    console.log(`Error for`);
-                    console.log(this.result);
-                }
-
-                if (format === "buffer") {
-                    reader.readAsArrayBuffer(file);
-                }
-                else {
-                    reader.readAsText(file);
-                }
-            });
-            }
-        },
-        err => { console.log("fail"); console.log(err); });
+                },
+                err => {
+                    console.log("fail");
+                    console.log(err);
+                    reject(`Error reading file ${fpath}`);
+                });
+        }
+    );
 }
